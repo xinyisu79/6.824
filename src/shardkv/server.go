594226ca0fcb2417c6613f082d250b56f0b56990
Jianfei Hu <hujianfei258@gmail.com>
2014-07-15 23:38:36 -0700

    some false thinking notes in server.go
    even though critical part of migration/reconfiguration is wrong, still
    retain it in this branch for later usage.

--- src/shardkv/server.go
+++ src/shardkv/server.go
@@ -43,16 +43,65 @@ Notes & Design (some from project description)
 
 5. after moving to new view, just leave shards not owing in new view there, undeleted, to simply implementation.
 
-6. Migrating Shard
-	Tick() periodically check whether the shard belongs itself, send shards to other group. But before sending content
-	and user seen[] map, should apply all operations before(include) paxos.Max(). This is similar in Get()/Put()
-	handler, has to try to append operation, instead of modify content[]map directly. Because due to unreliable
-	network, it's possible for one peers fail to get paxos instance decision.
-	On receiver side, could have map[gid] => processed_forward_config_num, to only process migration once,
-	but seems multiple times does not matter. :)
+//==================================================================================
+Even though the following analysis is wrong, still want to keep it in some git commit
+//==================================================================================
 
+
+6. Migrating Shard
+
+	terms: forward shard S from G1 to G2, client is ck, this called migration . tick() periodically check new config
+	from shardmaster.
+
+	a) actions before migrate
+	have to wait seq_num <= paxos.Max() to be applied before sending, ensuring sending shard content[] is updated.
+	(seq_num < Max() could have operation on S) it's like when applying Get/Put, instead of modifying content[] directly,
+	 append paxos log, and then apply. (reason: due to partition, this server may not see paxos decision on previous
+	 operations)
+
+	b) when to migrate
+	at first sight, it seems naturally to migrate during tick() config checking, however, it's not true.
+	G1 is running on other shards processing, and not get into tick() to migrate S to G2, before migration happends,
+	client request G2 for Put/Get. G2 fail to find entry in content[]. Wrong
+	luckily, client send request before do shardmaster.Query(-1) (see client.go) , so it would always have chance to
+	send request to Wrong group(that's responsible for S in old config). So when G1 saw ErrGroup,
+	have opportunities and should migrate S to G2. (corner case: in first time, ck.config[S] = 0,
+	ok; after getting first config, ck.config[S] = G1, but G1 not know about it, (not call tick() update config yet),
+	it's also ok to return ErrWrongGroup, as ck would still send request to G1, and the content[] is empty,
+	so does not matter.
+
+	c) at-most-once migration?
+	under above discussion, it's possible several severs in G1 all trying to migrate, but for replicas in G2,
+	should accept migration from G1 only once.
+	Senarios of multiple migrate: ck -> sr1 in G1, ErrGroup(not update UUID), unreliable, no response,
+	sr1 migrate S => G2; ck -> sr2@G1, try to forward again.
+	map[shard] => from_what_config_num_that_this_group_have_the_shard to record,
+	pigpack this number along with migration request
+
+	Should be at-most-once:
+	ck1(a) cause sr1 G1 => G2, unreliable; ck2(b) => G2 update (ck1/ck2 different keys, same shard);
+	ck1(a) => sr2 G2, migrate => G2. then update in ck2(b) would be lost.
+
+	d) accept all client[] status? (seen[], oldreplies[] map)
+	no. if current G2.seen[ck] > migration.seen[ck], not update, otherwise update and modify seen[] correspondingly.
+	This may cause modify seen[ck] that ck does not send to G2, (only request on Shard S should to G2 later) but does
+	not matter to keep it as each client has only one outstanding request
+
+
+
+7. shared paoxs?
+	according to code in test_test.go, shardmaster and k/v replica servers use seperate sets of paxos peers,
+	so reconfigure and put/get operation are sperately ordered, not in same log stream. So it's possible that cfg1 =>
+	cfg2, but servers/clients still use cfg1 to record. for correctness it's ok if consistency & orderings between
+	config and operations.
 
 Hint: Think about when it is ok for a server to give shards to the other server during view change.
+
+
+Plan:
+Firstly try simplest migration: sender/receiver both does not track shard they handled, always migrate when see
+ErrGroup
+
 
 
 */
@@ -98,6 +147,13 @@ type ShardKV struct {
 	seen map[string] int64 // seen outstanding client request, maintaining at-most-once semantics
 	replies map[string] string // map (client_name) => last outstanding reply result
 	processed int // up to this seq number, all has been applied in content dict{}
+	config shardmaster.Config
+}
+
+type MigratedShard struct {
+	Content map[string] string
+	Seen map[string] int64
+	Replies map[string] string
 }
 
 
@@ -165,31 +221,85 @@ func (kv *ShardKV)AddOp(op Op) string {
 
 
 
+func (kv *ShardKV) AcceptMigration(migration MigratedShard) error{
+	return nil
+}
+
+/**
+just need to do once waiting and apply operation whose seq_num < paxos.Max(); then sent the shard S to new group. No
+need to worry about this group G would have accept new op in before sending result, as once we have reach the new config
+where this group not responsible shard S, other replica in G would not accept operation for shard S anymore.
+*/
+func (kv *ShardKV) Migrate(shard int){
+	//waits for opeartions to finish before migration
+	//TODO: how to deal with unknown paxos instance? put up NO-OP?
+
+
+	migrated :=  MigratedShard{Content:map[string]string{},
+		Seen:map[string]int64{}, Replies:map[string]string{}}
+	for k := range kv.content{
+		if key2shard(k) == shard{
+			migrated.Content[k] = kv.content[k]
+		}
+	}
+	for c := range kv.seen{
+		migrated.Seen[c] = kv.seen[c]
+		migrated.Replies[c] = kv.replies[c]
+	}
+}
+
+
+func (kv *ShardKV) CheckOwnership(key string) Err{
+	shard := key2shard(key)
+	//should try to migrate shards here
+	if kv.config.Shards[shard] != kv.gid{
+		kv.Migrate(shard)
+		return ErrWrongGroup
+	}
+	return OK
+}
+
+
 func (kv *ShardKV) Get(args *GetArgs, reply *GetReply) error {
 	// Your code here.
-	reply.Err = OK
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
 	key, uuid, ck := args.Key, args.UUID, args.Me
-	result := kv.AddOp(Op{Type:GetOp, Key:key, UUID:uuid, Client:ck})
-	reply.Value = result
+	reply.Err = kv.CheckOwnership(key)
+	if reply.Err == OK {
+		result := kv.AddOp(Op{Type:GetOp, Key:key, UUID:uuid, Client:ck})
+		reply.Value = result
+	}
 	return nil
 }
 
 func (kv *ShardKV) Put(args *PutArgs, reply *PutReply) error {
 	// Your code here.
-	reply.Err = OK
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
 	key, value, dohash, uuid, ck := args.Key, args.Value, args.DoHash, args.UUID, args.Me
-	result := kv.AddOp(Op{PutOp, key, value, dohash, uuid, ck})
-	if dohash{
-		reply.PreviousValue = result
+	reply.Err = kv.CheckOwnership(key)
+	if reply.Err == OK {
+		result := kv.AddOp(Op{PutOp, key, value, dohash, uuid, ck})
+		if dohash{
+			reply.PreviousValue = result
+		}
 	}
 	return nil
 }
+
 
 //
 // Ask the shardmaster if there's a new configuration;
 // if so, re-configure.
 //
 func (kv *ShardKV) tick() {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	newcfg := kv.sm.Query(-1)
+	if newcfg.Num > kv.config.Num{
+		kv.config = newcfg
+	}
 }
 
 
@@ -217,6 +327,7 @@ func StartServer(gid int64, shardmasters []string,
 	kv.me = me
 	kv.gid = gid
 	kv.sm = shardmaster.MakeClerk(shardmasters)
+	kv.config = shardmaster.Config{Num:-1}
 
 	// Your initialization code here.
 	// Don't call Join().
